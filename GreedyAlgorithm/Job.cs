99c3eeb79325cc49207f7ee3dd587e9283e632a6
qwetry4444 <mishkin_mi@edu.surgu.ru>
2024-05-24 14:11:49 +0500

    brood

--- GreedyAlgorithm/Job.cs
+++ GreedyAlgorithm/Job.cs
@@ -10,18 +10,25 @@ namespace GreedyAlgorithm
     {
         public DateTime startTime;
         public DateTime endTime;
+        public double value;
         public double profit;
-        
-        public Job(DateTime startTime, DateTime endTime, double profit)
+
+        public Job(DateTime startTime, DateTime endTime, double value)
         {
             this.startTime = startTime;
             this.endTime = endTime;
-            this.profit = profit;
+            this.value = value;
+            this.profit = value / (endTime - startTime).TotalSeconds;
         }
 
         public static bool CompareJobs(Job job1, Job job2)
         {
             return job1.startTime.CompareTo(job2.startTime) == 1;
+        }
+
+        public string toString()
+        {
+            return $"Время начала: {startTime}; Время конца: {endTime}; Прибыль: {value}";
         }
     }
 
--- GreedyAlgorithm/Program.cs
+++ GreedyAlgorithm/Program.cs
@@ -1,6 +1,9 @@
 ﻿
 using System.Diagnostics;
 using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Linq;
+
 
 namespace GreedyAlgorithm
 {
@@ -8,81 +11,175 @@ namespace GreedyAlgorithm
     {
         static void Main(string[] args)
         {
+            //Job job1 = new Job(
+            //    new DateTime(1, 1, 1, hour: 10, minute: 0, second: 0), 
+            //    new DateTime(1, 1, 1, hour: 12, minute: 0, second: 0),
+            //    100);
+            //Job job2 = new Job(
+            //    new DateTime(1, 1, 1, hour: 11, minute: 0, second: 0),
+            //    new DateTime(1, 1, 1, hour: 12, minute: 0, second: 0),
+            //    200);
+            //Job job3 = new Job(
+            //    new DateTime(1, 1, 1, hour: 11, minute: 30, second: 0),
+            //    new DateTime(1, 1, 1, hour: 12, minute: 30, second: 0),
+            //    400);
+            //Job job4 = new Job(
+            //    new DateTime(1, 1, 1, hour: 12, minute: 0, second: 0),
+            //    new DateTime(1, 1, 1, hour: 13, minute: 0, second: 0),
+            //    500);
+
+            //List<Job> jobs = new List<Job>();
+            //jobs.Add(job1);
+            //jobs.Add(job2);
+            //jobs.Add(job3);
+            //jobs.Add(job4);
+
             Job job1 = new Job(
-                new DateTime(1, 1, 1, hour: 10, minute: 0, second: 0), 
-                new DateTime(1, 1, 1, hour: 12, minute: 0, second: 0),
-                100);
+    new DateTime(1, 1, 1, hour: 10, minute: 0, second: 0),
+    new DateTime(1, 1, 1, hour: 14, minute: 0, second: 0),
+    1000);
             Job job2 = new Job(
+                new DateTime(1, 1, 1, hour: 7, minute: 0, second: 0),
                 new DateTime(1, 1, 1, hour: 11, minute: 0, second: 0),
-                new DateTime(1, 1, 1, hour: 12, minute: 0, second: 0),
-                200);
+                900);
             Job job3 = new Job(
-                new DateTime(1, 1, 1, hour: 11, minute: 30, second: 0),
-                new DateTime(1, 1, 1, hour: 12, minute: 13, second: 0),
-                400);
-            Job job4 = new Job(
-                new DateTime(1, 1, 1, hour: 12, minute: 0, second: 0),
                 new DateTime(1, 1, 1, hour: 13, minute: 0, second: 0),
-                500);
+                new DateTime(1, 1, 1, hour: 17, minute: 0, second: 0),
+                800);
 
             List<Job> jobs = new List<Job>();
             jobs.Add(job1);
             jobs.Add(job2);
             jobs.Add(job3);
-            jobs.Add(job4);
-            List<Job> profitableJobs = new List<Job>(); 
-            profitableJobs = GetProfitableJobs(jobs);
+            List<Job> profitableJobs = new List<Job>();
+            //profitableJobs = GetProfitableJobs(jobs);
+            profitableJobs = GetProfitableJobsGreedy(jobs);
+            Console.WriteLine(GetMaxProfit(jobs));
+            //double profit = 0;
+            //Console.WriteLine("Подмножество работ, обеспечивающих максимальную прибыль");
+            //foreach (Job job in profitableJobs)
+            //{
+            //    Console.WriteLine(job.toString());
+            //    profit += job.value;
+            //}
+            //Console.WriteLine($"Суммарная прибыль = {profit}");
 
         }
 
-        public static List<Job> GetProfitableJobs(List<Job> jobs)
+        //public static List<Job> GetProfitableJobs(List<Job> jobs)
+        //{
+        //    double maxProfit = 0;
+        //    double currentProfit;
+        //    DateTime currentTime;
+
+        //    List<Job> jobsAnswer = [];
+        //    List<Job> profitableJobs = [];
+        //    Stack<Job> stack = new();
+
+        //    jobs.Sort((x, y) => DateTime.Compare(x.startTime, y.startTime));
+
+
+
+        //    for (int jobIndex = 0;  jobIndex < jobs.Count; jobIndex++)
+        //    {
+        //        stack.Push(jobs[jobIndex]);
+        //        profitableJobs[0] = jobs[jobIndex];
+        //        currentProfit = jobs[jobIndex].profit;
+        //        currentTime = jobs[jobIndex].endTime;
+
+        //        while (stack.Count > 0)
+        //        {
+        //            for (int nextJobIndex = jobIndex + 1;  nextJobIndex < jobs.Count; nextJobIndex++)
+        //            {
+        //                if (jobs[nextJobIndex].startTime > currentTime)
+        //                {
+        //                    stack.Push(jobs[nextJobIndex]);
+        //                    profitableJobs.Add(jobs[nextJobIndex]);
+        //                    currentProfit += jobs[nextJobIndex].profit;
+        //                    currentTime = jobs[nextJobIndex].endTime;
+        //                    break;
+        //                }
+        //                else
+        //                {
+        //                    if (currentProfit > maxProfit)
+        //                    {
+        //                        maxProfit = currentProfit;
+        //                        jobsAnswer = profitableJobs;
+        //                    }
+        //                    stack.Pop();
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return profitableJobs;
+        //}
+
+        public static List<Job> GetProfitableJobsGreedy(List<Job> jobs)
         {
-            double maxProfit = 0;
-            double currentProfit;
-            DateTime currentTime;
-
+            List<Job> jobsByTime = new List<Job>(jobs);
             List<Job> jobsAnswer = [];
-            List<Job> profitableJobs = [];
-            Stack<Job> stack = new();
 
-            jobs.Sort((x, y) => DateTime.Compare(x.startTime, y.startTime));
-
+            jobsByTime = jobs.OrderBy(job => job.startTime).ToList();
+            jobs = jobs.OrderByDescending(job => job.profit).ToList();
 
 
-            for (int jobIndex = 0;  jobIndex < jobs.Count; jobIndex++)
+            Job currentJob;
+            bool flag;
+            for (int i = 0; i < jobs.Count; i++)
             {
-                stack.Push(jobs[jobIndex]);
-                profitableJobs[0] = jobs[jobIndex];
-                currentProfit = jobs[jobIndex].profit;
-                currentTime = jobs[jobIndex].endTime;
-
-                while (stack.Count > 0)
+                currentJob = jobs[i];
+                flag = true;
+                for (int j = 0;  j < jobsAnswer.Count; j++)
                 {
-                    for (int nextJobIndex = jobIndex + 1;  nextJobIndex < jobs.Count; nextJobIndex++)
+                    if (!(currentJob.endTime <= jobsAnswer[j].startTime) && !(jobsAnswer[j].endTime <= currentJob.startTime))
                     {
-                        if (jobs[nextJobIndex].startTime > currentTime)
-                        {
-                            stack.Push(jobs[nextJobIndex]);
-                            profitableJobs.Add(jobs[nextJobIndex]);
-                            currentProfit += jobs[nextJobIndex].profit;
-                            currentTime = jobs[nextJobIndex].endTime;
-                            break;
-                        }
-                        else
-                        {
-                            if (currentProfit > maxProfit)
-                            {
-                                maxProfit = currentProfit;
-                                jobsAnswer = profitableJobs;
-                            }
-                            stack.Pop();
-                        }
+                        flag = false; break;
                     }
                 }
+                if (flag == true)
+                {
+                    jobsAnswer.Add(currentJob);
+                }
             }
-            return profitableJobs;
+
+            return jobsAnswer;
+        }
+
+
+        public static double GetMaxProfit(List<Job> jobs)
+        {
+            jobs = jobs.OrderBy(job => job.startTime).ToList();
+            return GetMaxProfitRecursive(jobs, 0, new List<Job>());
+        }
+
+        private static double GetMaxProfitRecursive(List<Job> jobs, int currentIndex, List<Job> selectedJobs)
+        {
+            if (currentIndex >= jobs.Count)
+            {
+                return selectedJobs.Sum(job => job.value);
+            }
+
+            double profitWithoutCurrent = GetMaxProfitRecursive(jobs, currentIndex + 1, selectedJobs);
+
+            bool canIncludeCurrent = true;
+            Job currentJob = jobs[currentIndex];
+            foreach (var job in selectedJobs)
+            {
+                if (!(currentJob.endTime <= job.startTime || job.endTime <= currentJob.startTime))
+                {
+                    canIncludeCurrent = false;
+                    break;
+                }
+            }
+
+            double profitWithCurrent = 0;
+            if (canIncludeCurrent)
+            {
+                List<Job> newSelectedJobs = new List<Job>(selectedJobs) { currentJob };
+                profitWithCurrent = GetMaxProfitRecursive(jobs, currentIndex + 1, newSelectedJobs);
+            }
+
+            return Math.Max(profitWithoutCurrent, profitWithCurrent);
         }
     }
-
- 
 }
